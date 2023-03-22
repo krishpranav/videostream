82a38330c70fec17a277c815ad9d079800b286ec
krishpranav <krisna.pranav@gmail.com>
2023-03-22 16:59:08 +0530

    filesys: scanner

--- filesys/scanner.go
+++ filesys/scanner.go
@@ -3,6 +3,9 @@ package filesys
 import (
 	"fmt"
 	"hash/fnv"
+	"log"
+	"os"
+	"path"
 	"path/filepath"
 	"sort"
 
@@ -12,21 +15,48 @@ import (
 func generateVideoKey(videoPath string) string {
 	hash := fnv.New32()
 	hash.Write([]byte(videoPath))
-
-	return fmt.Sprintf("%d%s", filepath.Ext(videoPath))
+	return fmt.Sprintf("%d%s", hash.Sum32(), filepath.Ext(videoPath))
 }
 
-func findVideoInPath(verbose bool, root string) {
+func findVideosInPath(verbose bool, root string,
+	videoDict data.VideoDict) []data.VideoDir {
+	if verbose {
+		log.Println("looking for videos in: " + root)
+	}
 
+	dirDict := make(map[string][]data.VideoInfo)
+
+	filepath.Walk(root,
+		func(pathStr string, info os.FileInfo, err error) error {
+			if data.IsStreamableVideoFormat(path.Ext(pathStr)) {
+				name := path.Base(pathStr)
+				dir := filepath.Dir(pathStr)
+				key := generateVideoKey(pathStr)
+				newVideo := data.VideoInfo{
+					FilePath: pathStr,
+					FileName: name,
+					Key:      key,
+				}
+
+				dirDict[dir] = append(dirDict[dir], newVideo)
+				videoDict[key] = newVideo
+			}
+			return err
+		})
+
+	vids := []data.VideoDir{}
+	for dirPath, videos := range dirDict {
+		vids = append(vids, data.NewVideoDir(dirPath, videos))
+	}
+	return vids
 }
 
 func FindAllVideos(config data.Config) (data.VideoDirectories, data.VideoDict) {
 	dirs := make(data.VideoDirectories, 0, 0)
 	videoDict := make(map[string]data.VideoInfo)
 	for _, pathStr := range config.VideoDirs {
-		dirs = append(dirs, findVideoInPath(config.Verbose, pathStr, config.VideoDirs[])...)
+		dirs = append(dirs, findVideosInPath(config.Verbose, pathStr, videoDict)...)
 	}
-
 	sort.Sort(dirs)
-	return dirs
+	return dirs, videoDict
 }
